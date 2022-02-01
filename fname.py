baa524e2ba0d25c2363b09c1ce58f312842e2c0d
Nikhil12354 <nikhilnarendra122@gmail.com>
2022-02-01 20:55:10 +0530

    updated fname.py code

--- fname.py
+++ fname.py
@@ -1,8 +1,8 @@
 #This is a simple python project to show first and last name
 
-fName = input("Please enter first name:").split()
-lName = input("Please enter last name:").split()
-age = input("How old are you?:").split()
+fName = input("Please enter first name:")
+lName = input("Please enter last name:")
+age = input("How old are you?:")
 
-print(f"My name is {fName} + {lName} & my age is {age}.")
+print(f"My name is {fName} {lName} & my age is {age}.")
 print()
