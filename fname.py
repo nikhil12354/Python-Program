4708c67bd40fd69966a889ca77083f259aa5be32
Nikhil12354 <nikhilnarendra122@gmail.com>
2022-02-03 14:54:37 +0530

    added isdigit

--- fname.py
+++ fname.py
@@ -4,5 +4,9 @@ fName = input("Please enter first name:")
 lName = input("Please enter last name:")
 age = input("How old are you?:")
 
-print(f"My name is {fName} {lName} & my age is {age}.")
-print()
+
+if age.isdigit():
+    print(f"My name is {fName} {lName} & my age is {age}.")
+
+else:
+    print("Age should be a number and not a letter.")
